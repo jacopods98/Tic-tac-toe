87044117a3f08b34fb872b6185e8a44696a57fcd
Jacopo De Santis <desantis.jacopo@icloud.com>
2021-04-13 12:14:08 +0200

    board made

--- js/gioco.js
+++ js/gioco.js
@@ -2,6 +2,17 @@ class Gioco{
     constructor(){
 
     }
+    draw(){
+        stroke("black");
+        strokeWeight(8);
+        fill("blue");
+        rect(o.ox, o.oy, o.size*3, o.size*3);
+        strokeWeight(4);
+        line(o.ox+o.size-1,o.oy,o.ox+o.size-1,o.oy+3*o.size);
+        line(o.ox+2*o.size,o.oy,o.ox+2*o.size,o.oy+3*o.size);
+        line(o.ox,o.oy+o.size-2,o.ox+3*o.size,o.oy+o.size-2);
+        line(o.ox,o.oy+2*o.size+1,o.ox+3*o.size,o.oy+2*o.size+1);
+    }
     reset(){
         
     }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 var gioco;
 var o={
-    CW=0,
-    CH=0,
+    CW:0,
+    CH:0,
+}
+
+function init(){
+    o.CW=windowWidth;
+    o.CH=windowHeight;
+    o.size=o.CW/5;
+    if(o.CH/4<o.size){
+        o.size=o.CH/4;
+    }
+    o.ox=(o.CW-o.size*4)/2
+    o.oy=(o.CH-o.size*3)/2
+}
+
+function windowResized() {
+    init();
+    resizeCanvas(o.CW, o.CH)
+    console.log("Uele")
+}
+
+function setup(){
+    init();
+    createCanvas(o.CW, o.CH).parent("canvas");
+}
+function draw(){
+    background(161, 201, 198)
+    gioco=new Gioco();
+    gioco.draw()
 }
